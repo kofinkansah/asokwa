409a0fc0870d747856ef4677717fc4e200fdfbfd
Kofi Appiah-Nkansah <kofinkansah@gmail.com>
2015-10-17 05:08:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Asokwa::Application.config.secret_key_base = '8fba9b7e55ebedd6ea0a1f8238db01ae8d5819caf3ebc50f1002fba253557330f93029db7bbf21c8a16620f2bf374865bc416aa3c902415160686a248b6e0cdb'
