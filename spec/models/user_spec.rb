2e1b9d561371fa1d5bffacdceb46e1abbeda9ee0
Kofi Appiah-Nkansah <kofinkansah@gmail.com>
2016-02-04 23:52:18 +0000

    Improved EMAIL_REGEX to reject foobar@email..com

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,25 +9,29 @@ RSpec.describe User, type: :model do
   	end
 
   	it "should have a name present" do #test2
-  		user = User.new(name: nil, email: 'opportunity@example.com')
+  		user = User.new(name: nil, email: 'opportunity@example.com',
+                      password: "foobar", password_confirmation: "foobar")
   		user.valid?
   		expect(user).not_to be_valid
   	end
 
   	it "should have an email present" do #test3
-  		user = User.new(name: "Oboo Emma", email: nil)
+  		user = User.new(name: "Oboo Emma", email: nil,
+                      password: "foobar", password_confirmation: "foobar")
   		user.valid?
   		expect(user).not_to be_valid
   	end
 
   	it "should not have a name over 50 characters" do #test4
-  		user = User.new(name: "a" * 51, email: 'tester@example.com')
+  		user = User.new(name: "a" * 51, email: 'tester@example.com',
+                      password: "foobar", password_confirmation: "foobar")
   		user.valid?
   		expect(user).not_to be_valid
   	end
 
   	it "should not have an email over 255 characters" do #test5
-  		user = User.new(name: "Zorro", email: 'a' * 244 +'@example.com')
+  		user = User.new(name: "Zorro", email: 'a' * 244 +'@example.com',
+                      password: "foobar", password_confirmation: "foobar")
   		user.valid?
   		expect(user).not_to be_valid
   	end
@@ -44,15 +48,17 @@ RSpec.describe User, type: :model do
 
   	it "should reject invalid email address" do #test7
   		invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-  													foo@bar_baz.com foo@bar+baz.com]
+  													foo@bar_baz.com foo@bar+baz.com foo@bar..com]
   		invalid_addresses.each do |invalid_address|
-  		user = User.new(name: "Xuru", email: invalid_address)
+  		user = User.new(name: "Xuru", email: invalid_address, password: "foobar",
+                      password_confirmation: "foobar")
   		expect(user).not_to be_valid, "#{invalid_address.inspect} should be invalid"
   		end #for invalid addresses block											
   	end #for test7
 	 
     it "should only accept unique emails" do #test8
-      user = User.new(name: "Johnson", email: 'tester@example.com')
+      user = User.new(name: "Johnson", email: 'tester@example.com',
+                      password: "foobar", password_confirmation: "foobar")
       duplicate_user = user.dup
       duplicate_user.email = user.email.upcase
       user.save
@@ -68,13 +74,15 @@ RSpec.describe User, type: :model do
     end
 
     it "should have a present password" do #test 9
-      user = User.new(password: "foobar", password_confirmation: "foobar")
+      user = User.new(name: "Johnson", email: 'tester@example.com',
+                      password: "foobar", password_confirmation: "foobar")
       user.password = user.password_confirmation = " " * 6
       expect(user).not_to be_valid
     end
 
     it "should have a password of a minimum 6 characters" do #test 10
-      user = User.new(password: "foobar", password_confirmation: "foobar")
+      user = User.new(name: "Johnson", email: 'tester@example.com',
+                      password: "foobar", password_confirmation: "foobar")
       user.password = user.password_confirmation = "a" * 5
       expect(user).not_to be_valid
     end
